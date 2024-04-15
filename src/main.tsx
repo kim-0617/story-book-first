dd9088d3fd7644375476545ee022857ee1e5b6e0
kim-0617 <kkk5993@naver.com>
2024-04-15 23:21:06 +0900

    notosanskr font download

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import "@fontsource/noto-sans-kr/400.css"; // Specify weight
+import "@fontsource/noto-sans-kr/700.css"; // Specify weight
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
